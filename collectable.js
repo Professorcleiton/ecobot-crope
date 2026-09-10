222cf1adba31c316191262d3c6d43a610c2cf896
Professorcleiton <159616293+Professorcleiton@users.noreply.github.com>
2026-09-10 09:40:58 -0300

    Update collectable.js

--- collectable.js
+++ collectable.js
@@ -12,7 +12,6 @@ class Lixo {
     fill(243, 156, 18);
     stroke(20);
     rect(this.x, this.y, this.tam, this.tam, 4);
-    // Símbolo de resíduo
     stroke(255);
     line(this.x + 4, this.y + 4, this.x + 16, this.y + 16);
     line(this.x + 16, this.y + 4, this.x + 4, this.y + 16);
@@ -32,5 +31,50 @@ class Lixo {
       return true;
     }
     return false;
+  }
+}
+
+// NOVO: Plataforma sólida suspensa
+class Plataforma {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+
+  desenhar() {
+    push();
+    fill(80, 90, 105);
+    stroke(40);
+    strokeWeight(2);
+    rect(this.x, this.y, this.w, this.h, 4);
+    pop();
+  }
+}
+
+// NOVO: Poça tóxica que reseta o robô
+class PocaAcido {
+  constructor(x, y, w) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = 15;
+  }
+
+  desenhar() {
+    push();
+    noStroke();
+    fill(231, 76, 60, 200); // Vermelho ácido
+    rect(this.x, this.y - 5, this.w, this.h, 6);
+    pop();
+  }
+
+  checarColisao(robo) {
+    return (
+      robo.x + robo.tam > this.x &&
+      robo.x < this.x + this.w &&
+      robo.y + robo.tam >= this.y
+    );
   }
 }
