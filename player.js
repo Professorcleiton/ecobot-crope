fe94a5c86e42f513b462a520516029ba666de000
Professorcleiton <159616293+Professorcleiton@users.noreply.github.com>
2026-09-10 09:45:31 -0300

    Update player.js

--- player.js
+++ player.js
@@ -2,16 +2,17 @@ class Robo {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.tam = 40;
+    this.tam = 36;
     this.velX = 0;
     this.velY = 0;
     this.velocidade = 4.5;
-    this.gravidade = 0.6;
-    this.forcaPulo = -11;
+    this.gravidade = 0.55;
+    this.forcaPulo = -11.5;
     this.noChao = false;
   }
 
-atualizar(chaoY, plataformas) {
+  atualizar(chaoY, plataformas) {
+    // Controles laterais (Setas ou A/D)
     if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) {
       this.velX = -this.velocidade;
     } else if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) {
@@ -22,26 +23,26 @@ atualizar(chaoY, plataformas) {
 
     this.x += this.velX;
 
-    // Gravidade
+    // Física e Gravidade
     this.velY += this.gravidade;
     this.y += this.velY;
 
     this.noChao = false;
 
-    // Colisão com o chão padrão
+    // Colisão com o piso base
     if (this.y + this.tam >= chaoY) {
       this.y = chaoY - this.tam;
       this.velY = 0;
       this.noChao = true;
     }
 
-    // Colisão com as plataformas suspensas
+    // Colisão com plataformas suspensas (apenas caindo sobre elas)
     for (let p of plataformas) {
       if (
         this.x + this.tam > p.x &&
         this.x < p.x + p.w &&
         this.y + this.tam >= p.y &&
-        this.y + this.tam <= p.y + 15 &&
+        this.y + this.tam <= p.y + 14 &&
         this.velY >= 0
       ) {
         this.y = p.y - this.tam;
@@ -65,23 +66,26 @@ atualizar(chaoY, plataformas) {
     stroke(20);
     strokeWeight(2);
 
-    // Corpo
+    // Chassi principal
     fill(72, 202, 228);
     rect(this.x, this.y, this.tam, this.tam, 8);
 
-    // Visor
-    fill(20, 20, 30);
-    rect(this.x + 6, this.y + 8, this.tam - 12, 14, 4);
+    // Visor digital
+    fill(15, 23, 42);
+    rect(this.x + 5, this.y + 7, this.tam - 10, 13, 4);
 
-    // Olhos
+    // Olhos LED
     fill(56, 239, 125);
-    ellipse(this.x + 14, this.y + 15, 5, 5);
-    ellipse(this.x + 26, this.y + 15, 5, 5);
+    noStroke();
+    ellipse(this.x + 12, this.y + 13, 4, 4);
+    ellipse(this.x + 24, this.y + 13, 4, 4);
 
     // Antena
-    line(this.x + 20, this.y, this.x + 20, this.y - 8);
-    fill(255, 107, 107);
-    circle(this.x + 20, this.y - 8, 6);
+    stroke(20);
+    strokeWeight(2);
+    line(this.x + 18, this.y, this.x + 18, this.y - 7);
+    fill(255, 90, 95);
+    circle(this.x + 18, this.y - 7, 5);
     pop();
   }
 }
