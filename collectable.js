72b6bdcca15168406422076b805aca3508eb3372
Professorcleiton <159616293+Professorcleiton@users.noreply.github.com>
2026-09-10 15:15:08 -0300

    Update collectable.js

--- collectable.js
+++ collectable.js
@@ -14,7 +14,6 @@ class Lixo {
     strokeWeight(1.5);
     rect(this.x, this.y, this.tam, this.tam, 4);
 
-    // Ícone de alerta/reciclagem
     stroke(255);
     line(this.x + 4, this.y + 4, this.x + 16, this.y + 16);
     line(this.x + 16, this.y + 4, this.x + 4, this.y + 16);
@@ -37,23 +36,35 @@ class Lixo {
   }
 }
 
+// Plataforma com suporte a movimento horizontal automático
 class Plataforma {
-  constructor(x, y, w, h) {
+  constructor(x, y, w, h, mover = false, alcance = 0) {
+    this.xInicial = x;
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
+    this.mover = mover;
+    this.alcance = alcance;
+    this.vel = 1.5;
+  }
+
+  atualizar() {
+    if (this.mover) {
+      this.x += this.vel;
+      if (this.x > this.xInicial + this.alcance || this.x < this.xInicial) {
+        this.vel *= -1;
+      }
+    }
   }
 
   desenhar() {
     push();
-    fill(71, 85, 105);
+    fill(this.mover ? 99 : 71, 102, this.mover ? 241 : 105);
     stroke(30, 41, 59);
     strokeWeight(2);
     rect(this.x, this.y, this.w, this.h, 4);
-
-    // Textura da plataforma
-    stroke(100, 116, 139);
+    stroke(148, 163, 184);
     line(this.x + 5, this.y + 3, this.x + this.w - 5, this.y + 3);
     pop();
   }
@@ -70,13 +81,14 @@ class PocaAcido {
   desenhar() {
     push();
     noStroke();
-    fill(239, 68, 68, 220); // Fluido tóxico avermelhado
+    fill(239, 68, 68, 220);
     rect(this.x, this.y - 4, this.w, this.h, 6);
-    
-    // Bolhas
-    fill(254, 202, 202, 180);
-    circle(this.x + 15, this.y, 4);
-    circle(this.x + this.w - 20, this.y + 2, 5);
+
+    // Bolha dinâmica
+    fill(254, 202, 202, 200);
+    let oscilacao = sin(frameCount * 0.1) * 3;
+    circle(this.x + 15, this.y + oscilacao, 5);
+    circle(this.x + this.w - 20, this.y - oscilacao, 4);
     pop();
   }
 
@@ -86,5 +98,35 @@ class PocaAcido {
       robo.x < this.x + this.w &&
       robo.y + robo.tam >= this.y - 4
     );
+  }
+}
+
+// Sistema de partículas para o rastro do robô
+class Particula {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.velX = random(-1, 1);
+    this.velY = random(-1, -0.2);
+    this.opacidade = 255;
+    this.tam = random(3, 6);
+  }
+
+  atualizar() {
+    this.x += this.velX;
+    this.y += this.velY;
+    this.opacidade -= 12;
+  }
+
+  desenhar() {
+    push();
+    noStroke();
+    fill(56, 239, 125, this.opacidade);
+    ellipse(this.x, this.y, this.tam);
+    pop();
+  }
+
+  terminou() {
+    return this.opacidade <= 0;
   }
 }
